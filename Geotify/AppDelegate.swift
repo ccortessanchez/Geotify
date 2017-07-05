ebbbf8166f8cb444d074f596f890e6db25f137e5
Carlos Cortés Sánchez <ccortes@gmv.es>
2017-07-05 14:03:58 +0200

    Notifying the user of geofence events

--- Geotify/AppDelegate.swift
+++ Geotify/AppDelegate.swift
@@ -32,11 +32,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
     locationManager.delegate = self
     locationManager.requestAlwaysAuthorization()
+    application.registerUserNotificationSettings(UIUserNotificationSettings(types: [.sound, .alert, .badge], categories: nil))
+    UIApplication.shared.cancelAllLocalNotifications()
     return true
   }
   
+  // MARK: Helper functions
   func handleEvent(forRegion region: CLRegion!) {
-    print("Geofence triggered!")
+    //print("Geofence triggered!")
+    // Show an alert if application is active
+    if UIApplication.shared.applicationState == .active {
+      guard let message = note(fromRegionIdentifier: region.identifier) else {return}
+      window?.rootViewController?.showAlert(withTitle: nil, message: message)
+    } else {
+      // Present a local notification
+      let notification = UILocalNotification()
+      notification.alertBody = note(fromRegionIdentifier: region.identifier)
+      notification.soundName = "Default"
+      UIApplication.shared.presentLocalNotificationNow(notification)
+    }
+  }
+  
+  func note(fromRegionIdentifier identifier: String) -> String? {
+    let savedItems = UserDefaults.standard.array(forKey: PreferencesKeys.savedItems) as? [NSData]
+    let geotifications = savedItems?.map { NSKeyedUnarchiver.unarchiveObject(with: $0 as Data) as? Geotification }
+    let index = geotifications?.index { $0?.identifier == identifier }
+    return index != nil ? geotifications?[index!]?.note : nil
   }
   
 }
